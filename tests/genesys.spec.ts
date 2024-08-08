49ff405f38c1ab459cfb926190da7a9df6916374
AMEEN KAPADIA <ameen.kapadia@solarturbines.com>
2024-08-08 13:10:40 -0700

    Final push

--- tests/genesys.spec.ts
+++ tests/genesys.spec.ts
@@ -1,7 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 
-//test case 1: Finding broken iamges
+/*test case 1: Finding broken iamges
+Test Scenario: To find broken images on loading screen
+Test Steps:
+  Navigate to https://magento.softwaretestingboard.com/
+  Gather all images
+  Check if src exists
+Prerequisites: none
+Browser: Chrome 
+Test Data: Images
+Expected/Intended Results: No broken images are found
+Actual Results: 14 broken images failed to load
+Test Status – Pass/Fail: Fail
+*/
+
 test('Find Broken Images', async ({ page }) => {
   //load page and wait for its content
   await page.goto("https://magento.softwaretestingboard.com/");
@@ -29,7 +42,25 @@ test('Find Broken Images', async ({ page }) => {
 })
 
 
-//test case 2: Creating an account
+/*test case 2: Creating an account
+Test Scenario: Creating an account with a gmail and password
+Test Steps:
+  Navigate to https://magento.softwaretestingboard.com/
+  Enter first name in "first name" field
+  Enter last name in "last name" field
+  Enter a registered email address in the email address input box (akapadia720sl@gmail.com)
+  Enter password (thisisaTest123!)
+  Confirm password (thisisaTest123!)
+  Click create account
+Prerequisites: A valid email address
+Browser: Chrome
+Test Data: email address (akapadia720sl@gmail.com), first name (Ameen), last name (Kapadia), password (thisisaTest123!)
+Expected/Intended Results: User is registered and returns to main page
+Actual Results: As Intended
+Test Status – Pass/Fail: Pass
+*/
+
+
 test('Creating an account', async ({ page }) => {
   //load page and wait for its content
   await page.goto("https://magento.softwaretestingboard.com/");
@@ -40,7 +71,7 @@ test('Creating an account', async ({ page }) => {
   //getting index x path of required fields
   await page.locator("(//input[@id='firstname'])[1]").type("Bob");
   await page.locator("(//input[@id='lastname'])[1]").type("Joe");
-  await page.locator("(//input[@id='email_address'])[1]").type("akapadia720s@gmail.com");
+  await page.locator("(//input[@id='email_address'])[1]").type("akapadia720sl@gmail.com");
   await page.locator("(//input[@id='password'])[1]").type("thisisaTest123!");
   await page.locator("(//input[@id='password-confirmation'])[1]").type("thisisaTest123!");
   
@@ -52,7 +83,23 @@ test('Creating an account', async ({ page }) => {
 
 })
 
-//test case 3: Signing into an account
+/*test case 3: Signing into an account
+Test Scenario: Signing into an account with a gmail and password
+Test Steps:
+  Navigate to https://magento.softwaretestingboard.com/
+  Click on the sign in link
+  Enter a registered email address (akapadia720sl@gmail.com) in the email address input box
+  Enter a registered password (thisisaTest123!)
+  Click sign in
+  Brought back to the main page
+Prerequisites: A valid email address and password
+Browser: Chrome
+Test Data: email (akapadia720sl@gmail.com) and password (thisisaTest123!)
+Expected/Intended Results: User is signed in successfully and returns to main page
+Actual Results: As Intended
+Test Status – Pass/Fail: Pass
+*/
+
 test('Signing into an Account', async ({ page }) => {
   //load page and wait for its content
   await page.goto("https://magento.softwaretestingboard.com/");
@@ -61,8 +108,8 @@ test('Signing into an Account', async ({ page }) => {
   await page.locator(':nth-match(:text("Sign In"), 1)').click();
 
   //gettong index x path of user input, and entering details, signing in
-  await page.locator("(//input[@id='email'])[1]").type("ameenkapadia1@gmail.com");
-  await page.locator("(//input[@id='pass'])[1]").type("Desk2018$");
+  await page.locator("(//input[@id='email'])[1]").type("akapadia720sl@gmail.com");
+  await page.locator("(//input[@id='pass'])[1]").type("thisisaTest123!");
   await page.locator("(//span[contains(text(),'Sign In')])[1]").click();
   await page.waitForTimeout(5000);
 
@@ -73,7 +120,34 @@ test('Signing into an Account', async ({ page }) => {
 
 })
 
-//test case 4: change password 
+/*test case 4: change password 
+Test Scenario: Change password for registered account
+Test Steps:
+  Navigate to https://magento.softwaretestingboard.com/
+  Click on the sign in link
+  Enter a registered email address in the email address input box (akapadia720sl@gmail.com)
+  Enter a registered password (thisisaTest123!)
+  Click sign in
+  Brought back to the main page
+  Navigate to "my account"
+  Click change password
+  Verify the name, address, and all fields are not empty
+  Verify that change password is checked
+  Enter new password (test12242#W)
+  Confirm new password (test12242#W)
+  Click save
+  Sign out automatically
+  Enter email (akapadia720sl@gmail.com)
+  Enter new password (test12242#W)
+  Click sign in
+Prerequisites: A valid registered account
+Browser: Chrome
+Test Data: email address (akapadia720sl@gmail.com), password (thisisaTest123!), new password (test12242#W)
+Expected/Intended Results: User changes password and signs in successfully after being logged out upon password change
+Actual Results: As Intended
+Test Status – Pass/Fail: Pass
+*/
+
 test('change password', async ({ page }) => {
 
   //load page and wait for its content
@@ -84,8 +158,8 @@ test('change password', async ({ page }) => {
 
   //assume user is signed in
   await page.locator("(//a[contains(text(),'Sign In')])[1]").click();
-  await page.locator("(//input[@id='email'])[1]").type("ameenkapadia1@gmail.com")
-  await page.locator("(//input[@id='pass'])[1]").type("Desk2018$")
+  await page.locator("(//input[@id='email'])[1]").type("akapadia720sl@gmail.com")
+  await page.locator("(//input[@id='pass'])[1]").type("thisisaTest123!")
 
   //view account
   await page.locator("(//button[@type='button'])[1]").click();
@@ -103,19 +177,40 @@ test('change password', async ({ page }) => {
   await page.waitForTimeout(2000);
 
   //sign in with new password
-  await page.locator("(//button[@title='Save'])[1]").type("ameenkapadia1@gmail.com");
+  await page.locator("(//button[@title='Save'])[1]").type("akapadia720sl@gmail.com");
   await page.locator("(//input[@id='pass'])[1]").type("test12242#W")
   await page.locator("(//button[@id='send2'])[1]").click();
 
 })
 
-//test case 5: adding clothes to cart
+/*test case 5: adding clothes to cart
+Test Scenario: Change password for registered account
+Test Steps:
+  Navigate to https://magento.softwaretestingboard.com/
+  Click on mens
+  Click tops
+  Select a color and size
+  Add item to cart
+  Click view cart
+  Verify that the subtotal and total item cost are matching
+  Go back to mens clothing
+  Add new item
+  Click on view and edit cart
+  Ensure price is updated
+Prerequisites: None
+Browser: Chrome
+Test Data: Mens jacket
+Expected/Intended Results: User adds item to cart and price is matching
+Actual Results: As Intended
+Test Status – Pass/Fail: Pass
+*/
 
 test('add to cart', async ({ page }) => {
 
   //load page and wait for its content
   await page.goto("https://magento.softwaretestingboard.com/");
   await page.waitForTimeout(2000);
+
   //navigating to mens clothigs page
   await page.locator("(//span[@class='ui-menu-icon ui-icon ui-icon-carat-1-e'])[4]").hover();
   await page.locator("(//a[@id='ui-id-17'])[1]").hover();
@@ -134,10 +229,25 @@ test('add to cart', async ({ page }) => {
   await page.waitForTimeout(2000);
   
   //ensure that subtotal and cart total match each other
+  
   //add new mens item and ensure that total is updated properly
 })
 
-//test case 6: using search box 
+/*test case 6: using search box 
+Test Scenario: Search for clothing
+Test Steps:
+  Navigates to https://magento.softwaretestingboard.com/
+  Enters "women's tee" into search box
+  Press enter
+  Verifies womens t shirts are loaded onto web page
+Prerequisites: Women's t shirts available
+Browser: Chrome
+Test Data: Women's clothing items available
+Expected/Intended Results: User searches for women's t shirts, resulting in new web page of only women's t shirts
+Actual Results: As Intended
+Test Status – Pass/Fail: Pass
+*/
+
 test('searching for clothes', async ({ page }) => {
 
   //load page and wait for its content
@@ -147,7 +257,8 @@ test('searching for clothes', async ({ page }) => {
   //entering into search input
   await page.locator("(//input[@id='search'])[1]").type("women's tee");
   await page.keyboard.press("Enter");
-  //check for items to be loaded onto page, only consisting of women's t shirts or related items
+  //check for items to be loaded onto page, consisting of women's t shirts or related items
+  
  
 })
 
@@ -155,6 +266,36 @@ test('searching for clothes', async ({ page }) => {
 
 //FUTURE TESTS
 
-//test case 7: only display clothes based on gender searched (search mens -> only mens clothing)
-//test case 8: advanced search feature
-//test case 9: ensure pricing changes upon quantity increase in view cart display mode
+/*test case 7: only display clothes based on gender searched (search mens -> only mens clothing)
+Test Scenario: Search for a gender's clothing, resulting only in the specified gender's clothing items
+Test Steps:
+  Navigates to https://magento.softwaretestingboard.com/
+  Enters "women's clothing" into search box
+  Press enter
+  Verifies only womens clothing is loaded
+Prerequisites: None
+Browser: Chrome
+Test Data: Womens clothing available
+Expected/Intended Results: User searches for women's clothing items, displaying only women's clothing items
+Actual Results: N/A
+Test Status – Pass/Fail: N/A
+*/
+
+/*test case 8: advanced search feature
+Test Scenario: Searching for specific item through advanced search feature
+Test Steps:
+  Navigates to https://magento.softwaretestingboard.com/
+  Click on advanced search
+  Enter product name in "product name" field
+  Enter SKU in 'SKU" field
+  Enter description in "description" field
+  Enter short description in "short description" field
+  Enter low end of price in first price input box
+  Enter high end of price in second price input box
+Prerequisites: An existing item
+Browser: Chrome
+Test Data: N/A
+Expected/Intended Results: User specifies item to be searched for with above descriptions and is resulted in a valid item or does not exist message
+Actual Results: N/A
+Test Status – Pass/Fail: N/A
+*/
